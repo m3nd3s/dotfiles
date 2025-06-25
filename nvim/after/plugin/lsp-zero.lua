62ca47c00de5b949ef1b269ceced0c1fb939517e
Almir Mendes <almir.mendes@rdstation.com>
2025-06-25 12:44:56 -0300

    Updates and fixes

--- nvim/after/plugin/lsp-zero.lua
+++ nvim/after/plugin/lsp-zero.lua
@@ -40,11 +40,25 @@ cmp.setup({
   sources = {
     {name = 'nvim_lsp'},
   },
+  mapping = cmp.mapping.preset.insert({
+    -- Navigate between completion items
+    ['<C-p>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
+    ['<C-n>'] = cmp.mapping.select_next_item({behavior = 'select'}),
+
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+
+    -- Scroll up and down in the completion documentation
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+  }),
   snippet = {
     expand = function(args)
       -- You need Neovim v0.10 to use vim.snippet
       vim.snippet.expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({}),
+    end
+  }
 })
